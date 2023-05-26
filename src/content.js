d2aeb704fbf6acfc880ff6a1d200f41494626c59
Vlado <htcvlado@gmail.com>
2023-05-26 02:31:07 +0200

    Separated discount value extraction logic

--- src/content.js
+++ src/content.js
@@ -1,64 +1,63 @@
 class DiscountFilter {
-    constructor(targetDiscount, itemsContainerSelector, itemDiscountSelector) {
-      this.targetDiscount = targetDiscount;
-      this.itemsContainer = document.querySelector(itemsContainerSelector);
-      this.itemDiscountSelector = itemDiscountSelector;
-    }
-  
-    filterItems() {
-      if (!this.itemsContainer) return;
-  
-      const items = Array.from(this.itemsContainer.children);
-  
-      items.forEach(item => {
-        if (this.targetDiscount == 0) {
-          this.displayItem(item);
-          return;
-        }
-  
-        const discountElement = item.querySelector(this.itemDiscountSelector);
-        if (!discountElement) {
-          this.hideItem(item);
-          return;
-        }
-  
-        const discountText = discountElement.textContent;
-        const discountValue = parseInt(discountText.split('-')[1], 10);
-  
-        discountValue >= this.targetDiscount ? this.displayItem(item) : this.hideItem(item);
-      });
-    }
-  
-    hideItem(item) {
-      item.style.display = 'none';
-    }
-  
-    displayItem(item) {
-      item.style.display = 'flex';
-    }
-  
-    setTargetDiscount(targetDiscount) {
-      this.targetDiscount = targetDiscount;
-      this.filterItems();
-    }
-  
-    observeItemsContainer() {
-      if (this.itemsContainer) {
-        const observer = new MutationObserver(this.filterItems.bind(this));
-        observer.observe(this.itemsContainer, { childList: true });
+  constructor(targetDiscount, itemsContainerSelector, discountValueExtractor) {
+    this.targetDiscount = targetDiscount;
+    this.itemsContainer = document.querySelector(itemsContainerSelector);
+    this.discountValueExtractor = discountValueExtractor;
+  }
+
+  filterItems() {
+    if (!this.itemsContainer) return;
+
+    const items = Array.from(this.itemsContainer.children);
+
+    items.forEach(item => {
+      if (this.targetDiscount == 0) {
+        this.displayItem(item);
+        return;
       }
+
+      this.discountValueExtractor(item) >= this.targetDiscount ? this.displayItem(item) : this.hideItem(item);
+    });
+  }
+
+  hideItem(item) {
+    item.style.display = 'none';
+  }
+
+  displayItem(item) {
+    item.style.display = 'flex';
+  }
+
+  setTargetDiscount(targetDiscount) {
+    this.targetDiscount = targetDiscount;
+    this.filterItems();
+  }
+
+  observeItemsContainer() {
+    if (this.itemsContainer) {
+      const observer = new MutationObserver(this.filterItems.bind(this));
+      observer.observe(this.itemsContainer, { childList: true });
     }
   }
-  
-  const discountFilter = new DiscountFilter(0, '.browsingitemcontainer', '.price-box__header-text');
-  
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'getDiscount') {
-      sendResponse({ discount: discountFilter.targetDiscount });
-    } else {
-      discountFilter.setTargetDiscount(request.discount);
-    }
-  });
-  
-  discountFilter.observeItemsContainer();
-  
+}
+
+const alzaItemsContainerClass = '.browsingitemcontainer';
+const alzaItemDiscountValueExtractor = (item) => {
+  const discountElement = item.querySelector('.price-box__header-text');
+  if (!discountElement) {
+    return null;
+  }
+  return parseInt(discountElement.textContent.split('-')[1], 10);
+};
+
+const discountFilter = new DiscountFilter(0, alzaItemsContainerClass, alzaItemDiscountValueExtractor);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'getDiscount') {
+    sendResponse({ discount: discountFilter.targetDiscount });
+  } else {
+    discountFilter.setTargetDiscount(request.discount);
+  }
+});
+
+discountFilter.observeItemsContainer();
