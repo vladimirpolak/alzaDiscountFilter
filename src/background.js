1e7ff305f445fda8e5463ebc5c3f03e0e998f400
Vlado <htcvlado@gmail.com>
2023-06-02 00:26:35 +0200

    removed background.js

--- src/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {discount: request.discount});
-      });
-    });
